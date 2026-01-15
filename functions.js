e8a6cd769dfad9a6c6c68f0474568c68c430610c
juliano <techjuliano@gmail.com>
2026-01-15 09:53:52 -0300

    video9 curso gas-advance

--- functions.js
+++ functions.js
@@ -1,9 +1,13 @@
 function doGet() {
-  return HtmlService.createTemplateFromFile("index")
-    .evaluate()
-    .setTitle("Agenda Google Apps Script");
+  return HtmlService.createTemplateFromFile('index').evaluate().setTitle('Agenda Google Apps Script');
 }
 
 function getDataHtml(name) {
   return HtmlService.createHtmlOutputFromFile(name).getContent();
+}
+
+function getContacts() {
+  let getSheet = SpreadsheetApp.openById('1GYKxNMN4ku4BGQy87Q0s94eeqbGArnlGLzYbs2BSqaQ').getActiveSheet();
+  let getData = getSheet.getDataRange().getValues();
+  return getData;
 }
